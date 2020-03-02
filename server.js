41b4992fdc8679b487d81001a49f71694f1b6e02
kazbag <45149138+kazbag@users.noreply.github.com>
2020-03-02 10:18:52 +0100

    added bearer prefix and preinitialized session and user for save some time

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const User = require('./models/User')
 const express = require('express')
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
@@ -8,9 +7,8 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-// only logged in users
-const sessions = []
-const users = []
+const sessions = [{ "token": "bearer abc", "name": "Andrzej" }]
+const users = [{ "name": "Andrzej", "password": "kleszcz23" }]
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
 
@@ -21,7 +19,7 @@ const secured = (req, res, next) => {
     if (!token) {
         res.status(401).send('access denied, no token');
     } else {
-        const session = sessions.find(session => session.token === token.replace("bearer ", ""))
+        const session = sessions.find(session => session.token === token)
         if (session) {
             next()
         } else {
@@ -29,11 +27,6 @@ const secured = (req, res, next) => {
         }
     }
 }
-
-app.get('/admin', secured, (req, res) => {
-    res.send('jesteś adminem')
-})
-
 
 app.get('/private', secured, (req, res) => {
     const token = req.header('Authorization');
@@ -50,7 +43,7 @@ app.post('/register', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-    const token = Math.floor((Math.random() * 1000000000)).toString(36)
+    const token = "bearer " + Math.floor((Math.random() * 1000000000)).toString(36)
     const { name, password } = req.body
     const user = users.find(user => user.name === name)
     if (user.password === password) {
