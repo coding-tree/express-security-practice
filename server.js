db1a6d17989a080f91b2359a0625067d54346313
kazbag <45149138+kazbag@users.noreply.github.com>
2020-03-03 11:55:53 +0100

    changed header token into cookie

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+// hostel (reverse proxy)https://github.com/typicode/hotel
+// pm2 - alternative for hostel (process manager)
 const express = require('express')
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const bcrypt = require('bcrypt')
 const port = 3000
 dotenv.config()
 
 const app = express()
-
+app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
@@ -19,11 +22,11 @@ app.listen(port, () => console.log(`app listening on port ${port}!`))
 
 // secured middleware
 const secured = (req, res, next) => {
-    const token = req.header('Authorization');
+    const token = req.cookies.Authorization
     if (!token) {
-        res.status(401).send('access denied, invalid token');
+        res.status(401).send('access denied, invalid token')
     } else {
-        const session = sessions.find(session => session.token === token)
+        const session = sessions.find(session => session.token === token.replace('bearer ', ''))
         if (session) {
             next()
         } else {
@@ -35,7 +38,7 @@ const secured = (req, res, next) => {
 app.get('/', (req, res) => res.send('witaj na stronie'))
 
 app.get('/private', secured, (req, res) => {
-    const token = req.header('Authorization')
+    const token = req.cookies.Authorization
     res.json({ token })
 })
 
@@ -46,16 +49,23 @@ app.post('/register', async (req, res) => {
     const user = {
         name, password: hashedPassword
     }
-    users.push(user);
+    users.push(user)
     res.json({ user })
 })
 
-app.post('/login', async (req, res) => {
-    const token = "bearer " + Math.floor((Math.random() * 1000000000)).toString(36)
+app.post('/login', async (req, res, next) => {
+    const token = req.cookies.Authorization
+    if (token === undefined) {
+        let newToken = Math.random().toString();
+        newToken = (Math.floor(Math.random() * 1000000000000).toString(36))
+        res.cookie('Authorization', newToken, { maxAge: 900000, httpOnly: true });
+    }
+
     const { name, password } = req.body
     const user = users.find(user => user.name === name)
-    const validPassword = await bcrypt.compare(password, user.password);
-    if (!validPassword) return res.status(400).send('invalid password');
+    const validPassword = await bcrypt.compare(password, user.password)
+    if (!validPassword) return res.status(400).send('invalid password')
     sessions.push({ token, user })
     res.json({ token, user })
+    next();
 })
