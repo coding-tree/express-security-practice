a1360ab78ec2ac1c92a3cecf2411db627b9f17b6
kazbag <45149138+kazbag@users.noreply.github.com>
2020-03-15 19:25:39 +0100

    added login and register handle

--- react-client/client/src/components/Form.js
+++ react-client/client/src/components/Form.js
@@ -5,19 +5,18 @@ import axios from 'axios';
 class Form extends React.Component {
     constructor(props) {
         super(props)
-
         this.state = {
             name: '',
-            password: ''
+            password: '',
+            form: "login"
         }
     }
     handleChanger = (e) => {
-        this.setState({ [e.target.name]: e.target.value })
+        this.setState({ [e.target.name]: e.target.value }, () => { console.log(this.state) })
     }
 
     registerHandler = (e) => {
         e.preventDefault()
-        console.log(this.state)
         axios.post('http://server.localhost/register', this.state)
             .then(response => {
                 console.log(response)
@@ -25,14 +24,35 @@ class Form extends React.Component {
                 console.log(error)
             })
     }
+    loginHandler = (e) => {
+        e.preventDefault()
+        axios.post('http://server.localhost/login', this.state)
+            .then(response => {
+                document.cookie = `Authorization=${response.data.token}`
+                console.log(response)
+            }).catch(error => {
+                console.log(error)
+            })
+    }
+
     render() {
         const { name, password } = this.state
         return (
-            <form onSubmit={this.registerHandler}>
+            <form onSubmit={this.state.form === 'login' ? this.loginHandler : this.registerHandler}>
+                <div className="labels">
+                    <label>
+                        <input type="radio" name="form" defaultChecked onChange={this.handleChanger} value="login" />
+                        Zaloguj
+                    </label>
+                    <label>
+                        <input type="radio" name="form" onChange={this.handleChanger} value="register" />
+                        Zarejestruj
+                    </label>
+                </div>
                 <input type="text" placeholder="login" name="name" value={name} onChange={this.handleChanger} />
                 <input type="password" placeholder="hasło" name="password" value={password} onChange={this.handleChanger} />
                 <div className="buttons">
-                    <button type="submit">Zarejestruj</button>
+                    <button type="submit">zaloguj</button>
                 </div>
             </form>
         )
