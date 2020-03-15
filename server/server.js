5ef04e31adf6a52566ebe7237c4db9e18f6010af
Damian Ospara <damian89m@gmail.com>
2020-03-15 22:46:33 +0100

    Add startup scripts to package.json. Refactor, clean up server.

--- server/server.js
+++ server/server.js
@@ -1,85 +1,95 @@
-// hostel (reverse proxy)https://github.com/typicode/hotel
-// pm2 - alternative for hostel (process manager)
-const express = require('express')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const bcrypt = require('bcrypt')
-const dotenv = require('dotenv').config()
-const port = process.env.PORT || 3000
-const cors = require('cors')
-const app = express()
-app.use(cors())
-app.use(cookieParser())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+require("dotenv").config();
 
-// database mock
-const sessions = [{ "token": "bearer abc", "name": "Andrzej" }]
-// valid password kleszcz232
-const users = [{ "name": "Andrzej2", "password": "$2b$10$96sk/L2XNNLuwi1iWTIcSOv42ArgH.IhYfTAOJsqt.MvYQydlVUW." }]
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const express = require("express");
+const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`app listening on port ${port}!`))
+const {
+  createSession,
+  fetchSession,
+  removeSession
+} = require("./SessionService");
+const { loginUser, registerUser } = require("./UserRepository");
 
-// secured middleware
+const app = express();
+
+app.use(cors());
+app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const cookieTokenExtractor = cookieName => (req, res, next) => {
+  req.token = req.cookies[cookieName];
+  next();
+};
+
+const authorizationHeaderTokenExtractor = (req, res, next) => {
+  const authorizationHeader = req.headers.authorization;
+  if (authorizationHeader) {
+    req.token = authorizationHeader.replace("bearer ", "");
+  }
+  next();
+};
+
 const secured = (req, res, next) => {
-    let token
-    if (req.headers.authorization) {
-        token = req.headers.authorization
+  let token = req.token;
+  if (!token) {
+    res.status(401).send("access denied, no token provided");
+  } else {
+    const session = fetchSession(token);
+    if (session) {
+      req.token = token;
+      req.session = session;
+      next();
     } else {
-        token = req.cookies.authorization
+      res.status(401).send("Invalid token");
     }
+  }
+};
 
-    if (!token) {
-        res.status(401).send('access denied, no token provided')
-    } else {
-        const session = sessions.find(session => session.token === token.replace('bearer ', ''))
-        if (session) {
-            next()
-        } else {
-            res.status(401).send("Invalid token")
-        }
-    }
-}
+const authorizationChain = [
+  cookieTokenExtractor("session"),
+  authorizationHeaderTokenExtractor,
+  secured
+];
 
-app.get('/', (req, res) => res.send('witaj na stronie'))
+app.get("/", (req, res) => res.send("witaj na stronie"));
 
-app.get('/private', secured, (req, res) => {
-    const token = req.cookies.authorization || req.headers.authorization
-    res.json({ token })
-})
+app.get("/private", authorizationChain, (req, res) => {
+  res.json({ session: req.session });
+});
 
-app.post('/register', async (req, res) => {
-    const { name, password } = req.body
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt)
-    const user = {
-        name, password: hashedPassword
-    }
-    users.push(user)
-    res.json({ user })
-    console.log(user);
-})
+app.post("/register", async (req, res) => {
+  const { name, password } = req.body;
+  const user = await registerUser(name, password);
+  res.json({ user: { name: user.name } });
+});
 
-app.post('/login', async (req, res, next) => {
-    let token = req.cookies.authorization || req.headers.authorization
-    if (!token) {
-        let newToken = (Math.floor(Math.random() * 1000000000000).toString(36))
-        res.cookie('authorization', newToken, { maxAge: 900000, httpOnly: true });
-        token = newToken
-        res.setHeader('authorization', token)
-    }
-    const { name, password } = req.body
-    const user = users.find(user => user.name === name)
-    if (!user) {
-        res.status(400).send('cannot find user')
-    }
-    const validPassword = await bcrypt.compare(password, user.password)
-    if (!validPassword) return res.status(400).send('invalid password')
-    sessions.push({ token, user })
-    res.json({ token, user, message: "everything is ok from server" })
-})
+app.post("/login", async (req, res) => {
+  const { name, password } = req.body;
+  const [user, error] = await loginUser(name, password);
+  if (error) {
+    res.status(400).send(error);
+  } else {
+    const token = createSession(user);
+    res.cookie("session", token, { maxAge: 900000, httpOnly: true });
+    res.json({
+      token,
+      user: {
+        name: user.name
+      },
+      message: "everything is ok from server"
+    });
+  }
+});
 
-app.post('/logout', async (req, res) => {
-    res.clearCookie('authorization')
-    res.redirect('/')
-})
+app.post("/logout", authorizationChain, async (req, res) => {
+  removeSession(req.token);
+  res.clearCookie("authorization");
+  res.redirect("/");
+});
+
+app.listen(port, () => console.log(`app listening on port ${port}!`));
