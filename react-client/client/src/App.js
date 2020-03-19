ac4bfd712f3cb7e06410e4f64020f01076892335
kazbag <45149138+kazbag@users.noreply.github.com>
2020-03-19 19:05:42 +0100

    welcome hooks

--- react-client/client/src/App.js
+++ react-client/client/src/App.js
@@ -1,38 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import Navbar from './components/Navbar';
 import Form from './components/Form';
 import Content from './components/Content';
 import LoggedPage from './components/LoggedPage';
+import Greetings from './components/Greetings';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import axios from 'axios';
 
-class App extends React.Component {
-  componentDidMount() {
-    // axios.
-  }
-  constructor(props) {
-    super(props)
-    this.state = {
-      isLogged: false
-    }
-  }
-  render() {
-
-    return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={Content} />
-            <Route path="/login" component={Form} />
-            <Route path="/register" component={Form} />
-          </Switch>
-          <LoggedPage />
-        </div>
-      </Router>
-    );
-  }
+const App = () => {
+  const [isLogged, setIsLogged] = useState(false);
+  useEffect(() => {
+    const token = document.cookie.replace("Authorization=", "")
+    if (!token) return
+    axios.post('http://server.localhost/check', { "token": token })
+      .then(() => {
+        setIsLogged(true)
+      }).catch(() => {
+        setIsLogged(false)
+      })
+  }, [])
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Greetings} />
+          <Route path="/login" component={Form} />
+          <Route path="/register" component={Form} />
+          <Route path="/private" component={isLogged ? Content : null} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ react-client/client/src/components/Greetings.js
@@ -0,0 +1,10 @@
+import React from 'react';
+class Greetings extends React.Component {
+    render() {
+        return (
+            <div style={{ color: 'white', textAlign: 'center' }}>hello</div>
+        )
+    }
+}
+
+export default Greetings;
